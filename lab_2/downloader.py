c331247d3bc1b6da8879d5ced9438bfb2c635ee8
Vera <veraqwer1234@gmail.com>
2024-11-24 06:10:27 +0300

    Downloading images added

---
+++ lab_2/downloader.py
@@ -0,0 +1,15 @@
+from icrawler.builtin import GoogleImageCrawler
+import os
+
+
+def download_images(keyword: str, image_dir: str, num: int) -> None:
+    """
+    Сохранение изображений из Google в указанную директорию.
+    :param keyword: ключевое слово для поиска
+    :param image_dir: путь к папке для сохранения
+    :param num: количество изображений
+    """
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    google_crawler = GoogleImageCrawler(storage={"root_dir": image_dir})
+    google_crawler.crawl(keyword, max_num=num)
