eb485b83067dfccd76d4186288e7d9d73f8d409b
Vera <veraqwer1234@gmail.com>
2024-11-25 11:34:52 +0300

    Code added to main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,5 +1,19 @@
+from annotation import create_annotation
+from downloader import download_images
+from iterator import ImageIterator
+from parser import parse
+
+
 def main():
-    pass
+    try:
+        keyword, image_dir, csv_path, num = parse()
+        download_images(keyword, image_dir, num)
+        create_annotation(csv_path, image_dir)
+        image_iterator = ImageIterator(csv_path)
+        for image in image_iterator:
+            print(image)
+    except Exception as exc:
+        print(f"Error: {exc}")
 
 
 if __name__ == "__main__":
