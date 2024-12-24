f95e2f292ecfc3c854873e137eea73d7b335093b
Vera <veraqwer1234@gmail.com>
2024-12-24 19:56:06 +0300

    Creating DataFrame

---
+++ lab_4/dataframe.py
@@ -0,0 +1,34 @@
+import cv2
+import pandas as pd
+
+
+def create_dataframe(csv_path: str) -> pd.DataFrame:
+    """
+    Создает дата фрейм из файла аннотации, считывает изображения и записывает их ширину, высоту и глубину,
+    печатает статистическую информацию.
+    :param csv_path: путь к файлу аннотации
+    :return: дата фрейм, содержащий пути к изображениям и свойства изображений
+    """
+    try:
+        df = pd.read_csv(csv_path, names=["absolute_path", "relative_path"])
+        width = []
+        height = []
+        channels = []
+
+        for img_path in df["absolute_path"]:
+            img = cv2.imread(img_path)
+            if img is not None:
+                width.append(img.shape[1])
+                height.append(img.shape[0])
+                channels.append(img.shape[2])
+            else:
+                raise FileNotFoundError(f"Image could not be read from {img_path}")
+
+        df["width"] = width
+        df["height"] = height
+        df["channels"] = channels
+        stats = df[["width", "height", "channels"]].describe()
+        print(stats)
+        return df
+    except FileNotFoundError:
+        print(f"Annotation file not found at {csv_path}")
